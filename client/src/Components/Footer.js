3530682ca837ba937f4e43c913e3381191aeda60
Alhawraa <66J208@utas.edu.om>
2025-11-11 09:43:17 +0400

    first uploud

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>Alhawraa - ©2023.PostIT. All Rights Reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
